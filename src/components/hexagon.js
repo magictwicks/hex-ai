c8853e96053b82fc2333d250b7aa4511061db8ae
SamTang1303 <samtang1303@gmail.com>
2023-05-30 11:19:37 -0700

    Refactored so that the hexImage state variable is no longer needed

--- src/components/hexagon.js
+++ src/components/hexagon.js
@@ -3,11 +3,21 @@ import blueHex from'../svg/hexagon-geometrical-shape-outline-svgrepo-com-blue.sv
 import redHex from '../svg/hexagon-geometrical-shape-outline-svgrepo-com-red.svg'
 import { useState } from 'react'
 
-export default function Hexagon( {blueTurn, takeTurn, color, setColor, hexClicked} ){ 
+export default function Hexagon( {color, hexClicked} ){ 
     
-
+    function toImage(colorValue){
+        if (colorValue > 0){
+            return blueHex;
+        }
+        if (colorValue < 0){
+            return redHex;
+        }
+        else {
+            return hex;
+        }
+    }
     return(
-        <img src={hex} alt="" class="hexagon" onClick={hexClicked}></img>
+        <img src={toImage(color)} alt="" class="hexagon" onClick={hexClicked}></img>
     );
     
 }
