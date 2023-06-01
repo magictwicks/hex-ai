e406accedc4dad3f17ef86db3265f950dfa61165
SamTang1303 <samtang1303@gmail.com>
2023-06-01 10:31:35 -0700

    Added game rules

--- src/components/rules.js
+++ src/components/rules.js
@@ -1,8 +1,15 @@
 export default function Rules () {
-  return (
-    <div>
-      <h2>These are the rules</h2>
-      <p>Hopefully they make sense.</p>
-    </div>
-  )
+    return (
+        <div>
+            <h2>Hex Rules:</h2>
+            <ol>
+                <li>Each player will have a color, either red or blue.</li>
+                <li>Blue moves first.</li>
+                <li>Every turn a player will claim an empty square for their color.</li>
+                <li>Two hexagons are considered connected if they are of the same color and share an edge.</li>
+                <li>Blue wins if they create a connected path of hexagons from the top edge of the board to the bottom.</li>
+                <li>Red wins if they create a connected path of hexagons from the left edge to the right.</li>
+            </ol>
+        </div>
+    )
 }
