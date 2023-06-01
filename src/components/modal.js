5da1b6564ccd4f3b6723533ec07593f5fde4f0ad
Levi Wicks <lwicks@westmont.edu>
2023-06-01 16:31:05 -0700

    added font and more css

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,10 +1,11 @@
+
 function Modal({content, show, hideModal}){
     const showClass = show ? "modal display-block" : "modal display-none"  
 
     return (
       <div class={showClass} >
           {content}
-          <button class="modal-close" onClick={hideModal}>Close</button>
+          <button class="modal-close" onClick={hideModal}>X</button>
       </div>
     );
 }
