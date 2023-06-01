7308f939fb278a2286b936572570cf7d6b81957a
Levi Wicks <lwicks@westmont.edu>
2023-06-01 11:32:07 -0700

    added transparent tiles

--- src/components/hexagon.js
+++ src/components/hexagon.js
@@ -1,9 +1,14 @@
 import hex from '../svg/hexagon-geometrical-shape-outline-svgrepo-com.svg'
 import blueHex from'../svg/hexagon-geometrical-shape-outline-svgrepo-com-blue.svg' 
 import redHex from '../svg/hexagon-geometrical-shape-outline-svgrepo-com-red.svg'
+import tRed from '../svg/transparent_red.svg'
+import tBlue from '../svg/transparent_blue.svg'
+import tPurp from '../svg/transparent_purple.svg'
+
+
 import { useState } from 'react'
 
-export default function Hexagon( {color, hexClicked} ){ 
+export default function Hexagon( {color, hexClicked, coords} ){ 
     
     function toImage(colorValue){
         if (colorValue > 0){
@@ -12,9 +17,24 @@ export default function Hexagon( {color, hexClicked} ){
         if (colorValue < 0){
             return redHex;
         }
-        else {
-            return hex;
+        const corners = [
+            "0,0",
+            "0,7",
+            "7,0",
+            "7,7"  
+        ];
+
+        if (corners.includes(coords.toString())){
+            return tPurp;
         }
+
+        if (coords[0] === 0 || coords[0] === 7){
+            return tBlue;
+        }
+        if (coords[1] === 0 || coords[1] === 7){
+            return tRed;
+        }
+        return hex;
     }
     return(
         <img src={toImage(color)} alt="" class="hexagon" onClick={hexClicked}></img>
